1b05732d81302d024e222b53f6f38fc1df2de5c3
haiping008 <haiping008@gmail.com>
2018-01-09 08:05:55 -0600

    Add ObjectHelper

---
+++ DotNetToolkit/ObjectHelper.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetToolkit
+{
+    public static class ObjectHelper
+    {
+        public static T ToObject<T>(this Object obj)
+        {
+            return JObject.FromObject(obj).ToObject<T>();
+        }
+
+        public static Object ToObject(this Object obj, Type type)
+        {
+            return JObject.FromObject(obj).ToObject(type);
+        }
+    }
+}
---
+++ DotNetToolkit/PageResultExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetToolkit
+{
+    public static class PageResultExtension
+    {
+        /// <summary>
+        /// Load data by pager
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pageResult"></param>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public static PageResult<T> LoadRecords<T>(this PageResult<T> pageResult, IQueryable<T> query)
+        {
+            int page = pageResult.Page;
+            int size = pageResult.Size;
+
+            pageResult.Total = query.Count();
+            pageResult.Items = query.Skip((page - 1) * size).Take(size).ToList();
+
+            return pageResult;
+        }
+    }
+
+    public class PageResult<T>
+    {
+        public PageResult()
+        {
+            Page = 1;
+            Size = 10;
+        }
+
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public int Total { get; set; }
+        public List<T> Items { get; set; }
+    }
+}
