4ce6aef310fc34525c9de44cec3d8ce26a7cac66
haiping008 <haiping008@gmail.com>
2018-01-09 23:42:08 -0600

    Added Log4NetHelper

---
+++ DotNetToolkit/Log4NetHelper.cs
@@ -0,0 +1,65 @@
+﻿using log4net;
+using log4net.Config;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DotNetToolkit
+{
+    public static class Log4NetHelper
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Log4NetHelper));
+
+        public static String configFilePath = "Settings\\log4net.config";
+
+        public static void Log(this Object log, CfLogLevel level = CfLogLevel.DEBUG)
+        {
+            if (!logger.Logger.Repository.Configured)
+            {
+                var assembly = Assembly.GetEntryAssembly();
+                var logRepository = LogManager.GetRepository(assembly);
+                XmlConfigurator.Configure(logRepository, new FileInfo(configFilePath));
+            }
+
+            string content = JsonConvert.SerializeObject(log,
+                Formatting.None,
+                new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                });
+
+            switch (level)
+            {
+                case CfLogLevel.DEBUG:
+                    logger.Debug(content);
+                    break;
+                case CfLogLevel.INFO:
+                    logger.Info(content);
+                    break;
+                case CfLogLevel.WARN:
+                    logger.Warn(content);
+                    break;
+                case CfLogLevel.ERROR:
+                    logger.Error(content);
+                    break;
+                case CfLogLevel.FATAL:
+                    logger.Fatal(content);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public enum CfLogLevel
+    {
+        ALL = 1,
+        DEBUG = 2,
+        INFO = 4,
+        WARN = 8,
+        ERROR = 16,
+        FATAL = 32,
+        OFF = 0
+    }
+}
