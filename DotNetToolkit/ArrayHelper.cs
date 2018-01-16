3bde25edb9427ce708fbec2676c44ed42a62dc2a
haiping008 <haiping008@gmail.com>
2018-01-15 23:38:35 -0600

    Addd more tools.

---
+++ DotNetToolkit/ArrayHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetToolkit
+{
+    public static class ArrayHelper
+    {
+        public static T GetRandom<T>(List<T> list)
+        {
+            int idx = new Random().Next(list.Count);
+            return list[idx];
+        }
+    }
+}
